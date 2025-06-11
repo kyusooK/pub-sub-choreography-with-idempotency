b491671cbeb76e0bd5dc67c7e11a43b2e982f6d8
kyusooK <rbtn110@uengine.org>
2025-06-11 01:48:58 +0000

    c

---
+++ delivery/src/main/java/choreography/with/idempotency/domain/Delivery.java
@@ -0,0 +1,67 @@
+package choreography.with.idempotency.domain;
+
+import choreography.with.idempotency.domain.DeliveryStarted;
+import choreography.with.idempotency.domain.DeliveryFailed;
+import choreography.with.idempotency.domain.DeliveryCancelled;
+import choreography.with.idempotency.DeliveryApplication;
+import javax.persistence.*;
+import java.util.List;
+import lombok.Data;
+import java.util.Date;
+
+@Entity
+@Table(name="Delivery_table")
+@Data
+
+public class Delivery  {
+    
+    @Id
+    // @GeneratedValue(strategy=GenerationType.AUTO) // FOCUS: disable auto-gen for key. 
+    private String orderId;
+
+    private String productId;
+    private String productName;
+    private Integer qty;
+    private String customerId;
+    private String address;
+    
+    private String status;
+
+
+    public static DeliveryRepository repository(){
+        DeliveryRepository deliveryRepository = DeliveryApplication.applicationContext.getBean(DeliveryRepository.class);
+        return deliveryRepository;
+    }
+
+
+    public static void startDelivery(OrderCreated orderCreated){
+
+        Delivery delivery = new Delivery();
+        delivery.setOrderId(String.valueOf(orderCreated.getId()));      // Prevent duplicate execution of the same message
+        delivery.setCustomerId(orderCreated.getCustomerId());
+        delivery.setProductId(orderCreated.getProductId());
+        delivery.setProductName(orderCreated.getProductName());
+        delivery.setQty(orderCreated.getQty());
+        repository().save(delivery);
+
+        DeliveryStarted deliveryStarted = new DeliveryStarted(delivery);
+        deliveryStarted.publishAfterCommit();        
+    }
+
+    public static void compensate(OrderRejected orderRejected){
+        
+        repository().findByOrderId(String.valueOf(orderRejected.getId())).ifPresent/*OrElse*/(delivery->{
+            new DeliveryCancelled(delivery).publishAfterCommit();
+            repository().delete(delivery);
+         }
+         
+        //, ()->{
+        //     throw new RuntimeException("No Delivery transaction is found for orderId" + orderRejected.getId());
+        //  }
+         );
+
+        
+    }
+
+
+}
